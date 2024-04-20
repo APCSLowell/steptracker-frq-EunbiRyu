f78000fba0c0b90876bb7ddb4aa9e66ecb5cd8c6
EunbiRyu <78942652+EunbiRyu@users.noreply.github.com>
2024-04-19 23:15:50 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -3,15 +3,15 @@ public class StepTracker
 {
  /* to be implemented here */
  
- private ArrayList<Integer> arrList();
+ private ArrayList <Integer> arrList();
  public StepTracker(int steps){
-  arrList = new ArrayList<Integer>();
+  arrList = new ArrayList <Integer>();
   arrList.add(steps);
  }
  public double averageSteps(){
   int sum=0;
   for(int i=0;i<arrList.size();i++){
-   sum += arrList.get();
+   sum += arrList.get(i);
   }
   return (double)sum/arrList.size();
  }
